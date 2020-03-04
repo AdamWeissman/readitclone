7ebf8d4ae21d6d804c63c775b40a9f4efcb6a043
Adam Weissman <47704263+AdamWeissman@users.noreply.github.com>
2020-03-04 07:44:06 -0500

    more tutorial

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,6 +1,24 @@
 class StoriesController < ApplicationController
+
   def index
     @current_time = Time.now
     @story = Story.order('RANDOM()').first #this would be great for generating flash cards... what's this text say?
   end
+
+  def new
+    @story = Story.new
+  end
+
+  def create
+    @story = Story.new(story_params)
+    @story.save
+    redirect_to stories_path
+  end
+
+private
+
+  def story_params
+    params.require(:story).permit(:name, :link)
+  end
+
 end
